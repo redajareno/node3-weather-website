5bd290a58c9b7c5df0c04b2c9456a1fdcd9e85cd
Reno Redaja <rredaja@radialspark.com>
2021-09-30 07:56:46 -1000

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=450441df21372c70f4b763735081e0bf&query=' + latitude + ',' + longitude + '&units=f';
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. ' + 'There is a ' + body.current.precip + '% chance of rain.')
+        }
+    })
+
+}
+
+module.exports = forecast;
